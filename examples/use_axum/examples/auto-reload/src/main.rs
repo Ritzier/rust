ff5e0b33127e6082f153cb22cb95e62521bd7ac3
ritzier <ritzierwheat@gmail.com>
2024-11-19 21:20:06 +0800

    feat: add example `auto-reload`

---
+++ examples/use_axum/examples/auto-reload/src/main.rs
@@ -0,0 +1,27 @@
+use axum::{response::Html, routing::get, Router};
+use listenfd::ListenFd;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    // build our application with a route
+    let app = Router::new().route("/", get(handler));
+
+    let mut listenfd = ListenFd::from_env();
+    let listener = match listenfd.take_tcp_listener(0).unwrap() {
+        Some(listener) => {
+            listener.set_nonblocking(true).unwrap();
+            TcpListener::from_std(listener).unwrap()
+        }
+        // Otherwise fall back to local listening
+        None => TcpListener::bind("127.0.0.1:3000").await.unwrap(),
+    };
+
+    // run it
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello, World!</h1>")
+}
