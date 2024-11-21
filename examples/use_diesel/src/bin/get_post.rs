02c73ae7a637cdb5ceeebd734e8d31fcd4e119d2
ritzier <ritzierwheat@gmail.com>
2024-11-21 19:11:16 +0800

    feat: add bin `get_post`

---
+++ examples/use_diesel/src/bin/get_post.rs
@@ -0,0 +1,28 @@
+use diesel::prelude::*;
+use std::env::args;
+use use_diesel::models::Post;
+use use_diesel::*;
+
+fn main() {
+    use use_diesel::schema::posts::dsl::posts;
+
+    let post_id = args()
+        .nth(1)
+        .expect("get_post requires a post id")
+        .parse::<i32>()
+        .expect("Invalid ID");
+
+    let connection = &mut establish_connection();
+
+    let post = posts
+        .find(post_id)
+        .select(Post::as_select())
+        .first(connection)
+        .optional(); // This allows for ruturning an Option<Post>, otherwise it will throw an error
+
+    match post {
+        Ok(Some(post)) => println!("Post with id: {} has a title: {}", post.id, post.title),
+        Ok(None) => println!("Unable to find post {}", post_id),
+        Err(_) => println!("An error occured while fetching post {}", post_id),
+    }
+}
