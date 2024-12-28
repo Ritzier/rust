7d12a3eaa6664b571994fabc9a6cd5d986038496
ritzier <ritzierwheat@gmail.com>
2024-12-28 14:52:23 +0800

    feat(use_leptos): add `server_fns_axum` example

---
+++ examples/use_leptos/examples/server_fns_axum/src/app.rs
@@ -0,0 +1,37 @@
+use leptos::config::LeptosOptions;
+use leptos::prelude::*;
+
+mod home;
+use home::HomePage;
+
+pub fn shell(options: LeptosOptions) -> impl IntoView {
+    view! {
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <meta charset="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <AutoReload options=options.clone() />
+                <HydrationScripts options />
+                <meta name="color-scheme" content="dark light" />
+                <link rel="shortcut icon" type="image/ico" href="/favicon.ico" />
+                <link rel="stylesheet" id="leptos" href="/pkg/server_fns_axum.css" />
+            </head>
+            <body>
+                <App />
+            </body>
+        </html>
+    }
+}
+
+#[component]
+pub fn App() -> impl IntoView {
+    view! {
+        <header>
+            <h1>"Server Function Demo"</h1>
+        </header>
+        <main>
+            <HomePage />
+        </main>
+    }
+}
